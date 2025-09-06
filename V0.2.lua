4194c3ed905bec057da2ee63e2b4432d7042d868
McChillington <114784049+McChillington@users.noreply.github.com>
2025-09-06 01:45:56 -0400

    Create V0.2.lua

---
+++ V0.2.lua
@@ -0,0 +1,24 @@
+-- get help at https://discord.gg/rB4x9dKgbU
+
+local Key = "" -- enter your key here
+local KeyLink = "" -- enter the link to get your key here
+
+local function ScriptHere() -- you will need to put your script on line 5, it can not surpass the end at the bottom
+
+end
+
+local note = "" -- Put a note here (Leave blank if you don't want one.)
+local scriptTitle = "" -- Put the title of your script (you can also leave this blank)
+
+local Discord = true -- Put "false" if you don't want the player to be able to copy a discord link, put true if you do.
+local Note = false
+
+-- Only do the following if you ticked discord true
+
+local discordLink = "" -- Put your discord link here
+
+-- Don't touch the rest of the script or it will break.
+
+-- 9/6/2025 1:35AM
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/McChillington/EZKEYS/refs/heads/main/V/V0.2Loader.lua",true))()
